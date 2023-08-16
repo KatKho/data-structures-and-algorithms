8656abaed43fe44b1f4d2517137b6a7ac7c52f72
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-08-15 18:35:18 -0700

    challenge 3

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -30,9 +30,7 @@ CHALLENGE 3
 Write a function named addQuestion that, given an array of strings, uses map to return a new array containing each string followed by a question mark character.
 ------------------------------------------------------------------------------------------------ */
 
-const addQuestion = (arr) => {
-  // Solution code here...
-};
+const addQuestion = (arr) => arr.map(value => value + '?');
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
